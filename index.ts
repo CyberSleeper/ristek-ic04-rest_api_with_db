37ac56f1de799c33e071d0ef6b8d709157dc8bd9
CyberSleeper <mahartha.gemilang@gmail.com>
2023-06-27 20:39:56 +0700

    done user CRUD

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import userRouter from './src/User/routes/user.route'
 
 const app = express();
 app.use(express.json());
+app.use('/users', userRouter);
 
 app.get('/', (req:Request, res:Response) => {
   res.status(200).json({ message: `Server listening at http://localhost:${PORT}` })
